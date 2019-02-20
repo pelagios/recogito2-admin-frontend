ade69c23230d0daf55808110a464cb36b9e912f8
Rainer Simon <rainer.simon@ait.ac.at>
2019-02-20 11:01:31 +0100

    Header style + collapsible nav menu

--- src/admin/users/App.jsx
+++ src/admin/users/App.jsx
@@ -3,12 +3,15 @@ import { render } from 'react-dom';
 
 import NavigationMenu from '../../common/components/NavigationMenu.jsx';
 
+import '../../common/styles/layout.scss';
+
 export default class App extends Component {
 
   render() {
     return (
       <div>
         <NavigationMenu />
+        <h1 className="page-title">User Management</h1>
       </div>
     );
   }
--- src/common/components/NavigationMenu.jsx
+++ src/common/components/NavigationMenu.jsx
@@ -6,32 +6,56 @@ import './NavigationMenu.scss';
 export default class NavigationMenu extends Component {
 
   state = {
-    collapsed: false
+    open: false
   };
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.onMousedown, false);
+    document.addEventListener('keydown', this.onKeydown, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.onMousedown, false);
+    document.removeEventListener('keydown', this.onKeydown, false);
+  }
+
+  onMousedown = (evt) => {
+    const isClickOutside = !this._node.contains(evt.target);
+    if (isClickOutside) this.setState({ open: false });
+  }
+
+  onKeydown= (evt) => {
+    if (evt.which === 27) this.setState({ open: false });
+  }
 
   toggle = () => {
     this.setState(prev => {
-      return { collapsed: !prev.collapsed }
+      return { open: !prev.open }
     });
   }
 
   render() {
     return (
-      <div className="nav-menu">
+      <div 
+        className="nav-menu"
+        ref={n => this._node = n}>
+
         <CSSTransition
           classNames="menu-options"
-          in={this.state.collapsed}
+          in={this.state.open}
           timeout={150}>
 
           <ul className="menu-options">
-            <li>Activity Dashboard</li>
-            <li>User Management</li>
-            <li>Entity Management</li>
-            <li>Housekeeping</li>
+            <li><a href="/admin">Activity Dashboard</a></li>
+            <li><a href="/admin/users">User Management</a></li>
+            <li><a href="/admin/entities">Entity Management</a></li>
+            <li><a href="/admin/maintenance">Housekeeping</a></li>
           </ul>
         </CSSTransition>
         
-        <div className="tab" onClick={this.toggle}></div>
+        <div className="tab" onClick={this.toggle}>
+          <span className="icon">&#xf0c9;</span>
+        </div>
       </div>
     );
   }
