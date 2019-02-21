92fe6b4ae40a34eab01c436cf54308bb2a6b8371
Rainer Simon <rainer.simon@ait.ac.at>
2019-02-21 15:18:08 +0100

    Minimal authority list

--- src/admin/authorities/App.jsx
+++ src/admin/authorities/App.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { render } from 'react-dom';
+import AuthorityList from './list/AuthorityList.jsx';
 import NavigationMenu from '../../common/components/navigationmenu/NavigationMenu.jsx';
 
 import './App.scss';
 
 export default class App extends Component {
 
+  state = {
+    authorities: []
+  }
+
+  componentDidMount() {
+    axios.get('/api/authorities/gazetteers')
+      .then(response => {
+        this.setState({ authorities: response.data });
+      });
+  }
+
   render() {
     return (
       <React.Fragment>
         <NavigationMenu />
-        <h1 className="page-title">Entity Management</h1>        
+        <h1 className="page-title">Entity Management</h1>      
+
+        <AuthorityList authorities={this.state.authorities} />  
+        
+        <div className="footer">
+          <button className="btn" onClick={this.props.onAddNew}>
+            <span class="icon">&#xf055;</span> Add New
+          </button>
+        </div>
       </React.Fragment>
     );
   }
---
+++ src/admin/authorities/list/AuthorityList.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+const fmt = new Intl.NumberFormat('en-US');
+
+export default class AuthorityList extends Component {
+
+  render() {
+    const rows = this.props.authorities.map(authority => 
+      <tr onClick={evt => this.props.onClick(authority)}>
+        {authority.conflicted ?
+          <td className="conflicted-warning"><span className="icon">&#xf071;</span></td> :
+          <td></td>
+        }
+        <td>{authority.identifier}</td>
+        <td>{authority.shortname}</td>
+        <td className="align-right">{fmt.format(authority.count)}</td>
+      </tr>
+    );
+
+    return (
+      <div className="authority-list">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Identifier</th>
+              <th>Name</th>
+              <th className="align-right">Records</th>
+            </tr>
+          </thead>
+
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    );
+  }
+
+}
